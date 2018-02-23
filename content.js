6ea99e118a22e1a506d5069dacdb06a189bae653
Ian Shaffer <ianrshaffer@gmail.com>
2018-02-23 14:32:53 -0800

    stuff

--- content.js
+++ content.js
@@ -7,23 +7,26 @@ for (var i = 0; i < elementsArray.length; i++) {
 };
 
 textToSpeechAjax("hey", function(response){
-    for (var i = 0; i < response.length; i++) {
-        console.log(i + ": " + response[i]);
-    }
-
-    var objectUrl = URL.createObjectURL(response);
+    console.log("hhh");
+    var blob = new Blob([response]);
+    console.log(blob);
+    var objectUrl = URL.createObjectURL(blob);
+    console.log(objectUrl);
     var audio = new Audio(objectUrl);
     audio.play();
+
 });
 
 function textToSpeechAjax(text, callback) {
+    console.log("eee");
     var url = "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/wav&text=" + text + "&voice=en-US_AllisonVoice"
     $.ajax({
         url: url,
         method: "GET",
         headers: {
             "Authorization": "Basic YzM4Mzk3Y2QtZTE5YS00M2FlLWJmNDEtMzc3YjRlMjc2NGIzOkFwNkpsN3daS1FFRA==",
-            "output": "speech.wav"
+            "output": "speech.wav",
+            "content-type": "binary"
         }
     }).then(function (response) {
         callback(response);
