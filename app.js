eef40f7719adb431e66f2d27438331f33fc228be
Charlie Oh <mun5424@gmail.com>
2018-02-23 12:50:08 -0800

    Jquery loaded

--- app.js
+++ app.js
@@ -5,3 +5,15 @@
             alert('test');
         }, false);
     }, false);
+
+    if (jQuery) {  
+        alert("jquery loaded");
+    } else {
+        // jQuery not loaded
+    }
+
+    $(document).ready(function() { 
+        $("#checkPage").on("click", function() {
+           alert("YAY!"); 
+        });
+    }); 
