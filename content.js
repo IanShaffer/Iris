0cb299954cc646a8f97fc14c6fb8e2bf90092b39
Ian Shaffer <ianrshaffer@gmail.com>
2018-02-28 13:31:42 -0800

    refactor

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
 
 // window variables
-var audio = new Audio();
+// content
 var block = false;
 var WAIT_TIME = 500;
-var googleTextToSpeech = false;
 var currentElement;
 var lastPlayedElement;
+// background
+var audio = new Audio();
+var googleTextToSpeech = false;
 var chosenLanguage = "english";
 var languages = {
     english: {
@@ -38,65 +40,13 @@ var languages = {
     }
 };
 
-
-$.ajaxTransport("+binary", function (options, originalOptions, jqXHR) {
-    // check for conditions and support for blob / arraybuffer response type
-    if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob))))) {
-        return {
-            // create new XMLHttpRequest
-            send: function (headers, callback) {
-                // setup all variables
-                var xhr = new XMLHttpRequest(),
-                    url = options.url,
-                    type = options.type,
-                    async = options.async || true,
-                    // blob or arraybuffer. Default is blob
-                    dataType = options.responseType || "blob",
-                    data = options.data || null,
-                    username = options.username || null,
-                    password = options.password || null;
-
-                xhr.addEventListener('load', function () {
-                    var data = {};
-                    data[options.dataType] = xhr.response;
-                    // make callback and send data
-                    callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
-                });
-
-                xhr.open(type, url, async, username, password);
-
-                // setup custom headers
-                for (var i in headers) {
-                    xhr.setRequestHeader(i, headers[i]);
-                }
-
-                xhr.responseType = dataType;
-                xhr.send(data);
-            },
-            abort: function () {
-                jqXHR.abort();
-            }
-        };
-    }
-});
-
 var elementsArray = document.getElementsByTagName('*');
 for (var i = 0; i < elementsArray.length; i++) {
     elementsArray[i].addEventListener("focus", function () {
         var value = this.innerHTML;
-        chrome.storage.sync.get('language', function (items) {
-            chosenLanguage = items.language;
-            // if English
-            if (!languages[chosenLanguage].modelId && value) {
+            if (value) {
                 playBlob(value);
-            // if not English
-            } else if (languages[chosenLanguage].modelId && value) {
-                translateAjax(value, function (response) {
-                    var spanishText = response.translations[0].translation;
-                    playBlob(spanishText);
-                });
             }
-        });
     });
     elementsArray[i].addEventListener("mouseleave", function (e) {
         currentElement = "";
@@ -139,7 +89,7 @@ for (var i = 0; i < elementsArray.length; i++) {
                     value = "Link to ";
                     value += element.title;
                 } else {
-                    value = "Unkown link";
+                    value = "Unknown link";
                 }
                 break;
             case "H1","H2","H3","H4":
@@ -154,53 +104,39 @@ for (var i = 0; i < elementsArray.length; i++) {
                 value = element.innerText;
         } 
         
-        // get the language set in the chrome extension modal
-        chrome.storage.sync.get('language', function (items) {
-            chosenLanguage = items.language;
-            
-            // if a sound has started playing within the WAIT_TIME interval, then queue up the current event
-            if (block && value) {
-                setTimeout(function () {
-                    // if the mouse is still over the element even after its queue time
-                    if (currentElement === element) {
-                        // if the current queued event is not blocked by a previously queued event and its not repeating the last played element
-                        if (!block && element !== lastPlayedElement) {
-                            console.log("Element:", element);
-                            // block further events for the WAIT_TIME interval (the blocked events will be queued)
-                            block = true;
-                            // unblock events after WAIT_TIME interval
-                            setTimeout(function () {
-                                block = false;
-                            }, WAIT_TIME);
-                            // let the browser know that this element is the most recently played element so its not replayed
-                            lastPlayedElement = element;
-                            // if English
-                            if (!languages[chosenLanguage].modelId && value) {
-                                playBlob(value);
-                            // if not English
-                            } else if (languages[chosenLanguage].modelId && value) {
-                                translateAjax(value, function (response) {
-                                    var spanishText = response.translations[0].translation;
-                                    playBlob(spanishText);
-                                });
-                            }
-                        }
+        // if a sound has started playing within the WAIT_TIME interval, then queue up the current event
+        if (block && value) {
+            setTimeout(function () {
+                // if the mouse is still over the element even after its queue time
+                if (currentElement === element) {
+                    // if the current queued event is not blocked by a previously queued event and its not repeating the last played element
+                    if (!block && element !== lastPlayedElement) {
+                        console.log("Element:", element);
+                        // block further events for the WAIT_TIME interval (the blocked events will be queued)
+                        block = true;
+                        // unblock events after WAIT_TIME interval
+                        setTimeout(function () {
+                            block = false;
+                        }, WAIT_TIME);
+                        // let the browser know that this element is the most recently played element so its not replayed
+                        lastPlayedElement = element;
+                        playBlob(value);
                     }
-                }, WAIT_TIME);
-            // if no sound has been played in the last WAIT_TIME amount of time
-            } else if (value) {
-                console.log("Element:", element);
-                // block further events for the WAIT_TIME interval (the blocked events will be queued)
-                block = true;
-                // unblock events after WAIT_TIME interval
-                setTimeout(function () {
-                    block = false;
-                }, WAIT_TIME);
-                // let the browser know that this element is the most recently played element so its not replayed
-                lastPlayedElement = element;
-                playBlob(value);
-            }
-        });
+                }
+            }, WAIT_TIME);
+        // if no sound has been played in the last WAIT_TIME amount of time
+        } else if (value) {
+            console.log("Element:", element);
+            // block further events for the WAIT_TIME interval (the blocked events will be queued)
+            block = true;
+            // unblock events after WAIT_TIME interval
+            setTimeout(function () {
+                block = false;
+            }, WAIT_TIME);
+            // let the browser know that this element is the most recently played element so its not replayed
+            lastPlayedElement = element;
+            playBlob(value);
+        }
     });
 }
 
@@ -225,30 +161,35 @@ function playBlob(text)
         {
             text = text.substring(0,20);
         }
-        // if chosen language is not english, translate text to foreign language
-        if (languages[chosenLanguage].modelId) {
-            translateAjax(text, function (response) {
-                var spanishText = response.translations[0].translation;
-                textToSpeechAjax(spanishText, function (response) {
+        text = text.replace(/</g, ' less than symbol ');
+        text = text.replace(/>/g, ' greater than symbol ');
+
+        chrome.storage.sync.get('language', function (items) {
+            if (items.language) {
+                chosenLanguage = items.language;
+            }
+
+            // if chosen language is not english, translate text to foreign language
+            if (languages[chosenLanguage].modelId) {
+                translateAjax(text, function (response) {
+                    var spanishText = response.translations[0].translation;
+                    textToSpeechAjax(spanishText, function (response) {
+                        var blob = new Blob([response], { "type": "audio/wav" });
+                        var objectUrl = window.URL.createObjectURL(blob);
+                        audio.src = objectUrl;
+                        audio.play();
+                    });
+                });
+            // if english, don't translate
+            } else {
+                textToSpeechAjax(text, function (response) {
                     var blob = new Blob([response], { "type": "audio/wav" });
                     var objectUrl = window.URL.createObjectURL(blob);
                     audio.src = objectUrl;
-                    var isPlaying = audio.currentTime > 0 && !audio.paused && !audio.ended && audio.readyState > 2;
-                    if (!isPlaying)
-                        audio.play();
-                });
-            });
-        // if english, don't translate
-        } else {
-            textToSpeechAjax(text, function (response) {
-                var blob = new Blob([response], { "type": "audio/wav" });
-                var objectUrl = window.URL.createObjectURL(blob);
-                audio.src = objectUrl;
-                var isPlaying = audio.currentTime > 0 && !audio.paused && !audio.ended && audio.readyState > 2;
-                if (!isPlaying)
                     audio.play();
-            });
-        }
+                });
+            }
+        });
     });
 }
 
@@ -264,11 +205,9 @@ function textToSpeechAjax(text, callback) {
             "Authorization": TEXT_TO_SPEECH_AUTH,
             "output": "speech.wav",
             "Access-Control-Allow-Origin": "*",
-            "Content-Type": "text/plain"
+            "Content-Type": "application/json"
         },
-        data: {
-            text: text
-        },
+        data: JSON.stringify({text: text}),
         dataType: "binary",
         responseType: "arraybuffer"
     }).then(function (response) {
@@ -342,3 +281,43 @@ function detectLanguageAjax(text, callback) {
     });
 };
 
+$.ajaxTransport("+binary", function (options, originalOptions, jqXHR) {
+    // check for conditions and support for blob / arraybuffer response type
+    if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob))))) {
+        return {
+            // create new XMLHttpRequest
+            send: function (headers, callback) {
+                // setup all variables
+                var xhr = new XMLHttpRequest(),
+                    url = options.url,
+                    type = options.type,
+                    async = options.async || true,
+                    // blob or arraybuffer. Default is blob
+                    dataType = options.responseType || "blob",
+                    data = options.data || null,
+                    username = options.username || null,
+                    password = options.password || null;
+
+                xhr.addEventListener('load', function () {
+                    var data = {};
+                    data[options.dataType] = xhr.response;
+                    // make callback and send data
+                    callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+                });
+
+                xhr.open(type, url, async, username, password);
+
+                // setup custom headers
+                for (var i in headers) {
+                    xhr.setRequestHeader(i, headers[i]);
+                }
+
+                xhr.responseType = dataType;
+                xhr.send(data);
+            },
+            abort: function () {
+                jqXHR.abort();
+            }
+        };
+    }
+});
