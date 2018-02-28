0450e6b00d150d114e0baa36d8fbc67967946c70
Charlie Oh <mun5424@gmail.com>
2018-02-28 10:28:04 -0800

    added a limit of 20 chars

--- content.js
+++ content.js
@@ -209,6 +209,10 @@ function playBlob(text)
         if (!text || isOn === false) {
             return;
         }
+        if (text.length > 20)
+        {
+            text = text.substring(0,20);
+        }
         if (languages[chosenLanguage].modelId) {
             translateAjax(text, function (response) {
                 var spanishText = response.translations[0].translation;
@@ -238,7 +242,6 @@ var TEXT_TO_SPEECH_AUTH = "Basic YjZhM2YxNWUtMjhmNi00OTc4LTk1YWMtOTQwZjI3MGY4MTE
 var TRANSLATION_AUTH = "Basic M2VhMWQwOTctZDRhZS00MzAwLTllN2MtNGQ1MmQ1ZGRmNWNjOkxwQ2RUWGdyUU02Vg==";
 
 function textToSpeechAjax(text, callback) {
-    text = text.substring(0,20);
     var url = "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/wav&voice=" + languages[chosenLanguage].voice;
     $.ajax({
         url: url,
@@ -263,7 +266,6 @@ function textToSpeechAjax(text, callback) {
 };
 
 function translateAjax(text, callback) {
-    text = text.substring(0, 20);
     var url = "https://gateway.watsonplatform.net/language-translator/api/v2/translate";
     $.ajax({
         url: url,
